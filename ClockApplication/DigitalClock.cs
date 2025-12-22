93e374673fc101289b132c0fc9d911b9b06b3bd5
Admin <Admin@DESKTOP-8URQBFL>
2025-12-22 20:27:22 +0500

    AnalogClock DONE

---
+++ ClockApplication/DigitalClock.cs
@@ -0,0 +1,82 @@
+﻿using ClockApplication;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace ClockApplication
+{
+	public partial class DigitalClock : UserControl
+	{
+		private Timer timer;
+		private Label labelTime;
+		public DigitalClock()
+		{
+			InitializeComponent();
+			//this.MaximizeBox = false;
+			//this.MinimizeBox = false;
+
+			//void SetVisibility(bool visible)
+			//{
+			//	cbShowDate.Visible = visible;
+			//	cbShowWeekDay.Visible = visible;
+			//	btnHideControls.Visible = visible;
+			//	this.ShowInTaskbar = visible;
+			//	this.FormBorderStyle = visible ? FormBorderStyle.FixedSingle : FormBorderStyle.None;
+			//	this.TransparencyKey = visible ? Color.Empty : this.BackColor;
+			//}
+
+			this.DoubleBuffered = true;
+			labelTime = new Label
+			{
+				Font = new Font("Consolas", 24, FontStyle.Bold),
+				ForeColor = Color.Navy,
+				TextAlign = ContentAlignment.MiddleCenter,
+				Dock = DockStyle.Fill
+			};
+			this.Controls.Add(labelTime);
+
+			timer = new Timer { Interval = 1000 };
+			timer.Tick += Timer_Tick;
+		}
+		private void Timer_Tick(object sender, EventArgs e)
+		{
+			labelTime.Text = DateTime.Now.ToString
+				(
+					"hh:mm:ss tt",
+					System.Globalization.CultureInfo.InvariantCulture
+				);
+		}
+		//if (cbShowDate.Checked)
+		//	labelTime.Text += $"\n{DateTime.Now.ToString("yyyy.MM.dd")}";
+		//if (cbShowWeekDay.Checked)
+		//	labelTime.Text += $"\n{DateTime.Now.DayOfWeek}";
+		//notifyIcon.Text = labelTime.Text;
+		public void Start() => timer.Start();
+		public void Stop() => timer.Stop();
+
+
+		//private void btnHideControls_Click(object sender, EventArgs e)
+		//{
+		//	SetVisibility(false);
+		//}
+
+		//private void labelTime_MouseHover(object sender, EventArgs e)
+		//{
+		//	SetVisibility(true);
+		//}
+
+		//private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+		//{
+		//	this.TopMost = true;
+		//	this.TopMost = false;
+		//}
+	}
+}
+
---
+++ ClockApplication/commonWindow.Designer.cs
@@ -0,0 +1,88 @@
+﻿using System.Windows.Forms;
+
+namespace ClockApplication
+{
+	partial class commonWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControl));
+
+
+			// 
+			// panelDisplay
+			//
+			this.panelDisplay = new System.Windows.Forms.Panel();
+			this.panelDisplay.BackColor = System.Drawing.Color.FromArgb(240, 240, 255);
+			this.panelDisplay.Location = new System.Drawing.Point(12, 12);
+			this.panelDisplay.Name = "panelDisplay";
+			this.panelDisplay.Size = new System.Drawing.Size(460, 460);
+			this.panelDisplay.TabIndex = 0;
+
+			//
+			// contextMenuClock
+			//
+			this.contextMenuClock = new System.Windows.Forms.ContextMenuStrip();
+			this.toolStripAnalog = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripDigital = new System.Windows.Forms.ToolStripMenuItem();
+
+			// contextMenuClock
+			this.contextMenuClock.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+				this.toolStripAnalog,
+				this.toolStripDigital});
+			this.contextMenuClock.Name = "contextMenuClock";
+			this.contextMenuClock.Size = new System.Drawing.Size(167, 70);
+
+			// toolStripAnalog
+			this.toolStripAnalog.Name = "toolStripAnalog";
+			this.toolStripAnalog.Size = new System.Drawing.Size(166, 22);
+			this.toolStripAnalog.Text = "Аналоговые часы";
+			// toolStripDigital
+			this.toolStripDigital.Name = "toolStripDigital";
+			this.toolStripDigital.Size = new System.Drawing.Size(166, 22);
+			this.toolStripDigital.Text = "Цифровые часы";
+
+			// MainForm
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(484, 484);
+			this.Controls.Add(this.panelDisplay);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.Text = "Часы";
+			this.ContextMenuStrip = this.contextMenuClock;
+			this.ResumeLayout(false);
+
+		}
+		#endregion
+		private System.Windows.Forms.Panel panelDisplay;
+		private System.Windows.Forms.ContextMenuStrip contextMenuClock;
+		private System.Windows.Forms.ToolStripMenuItem toolStripAnalog;
+		private System.Windows.Forms.ToolStripMenuItem toolStripDigital;
+	}
+}
+
