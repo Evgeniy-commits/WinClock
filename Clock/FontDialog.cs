67986a78d9f6ded9b91d02567076986fd553ee77
Admin <Admin@DESKTOP-8URQBFL>
2026-01-11 22:52:06 +0500

    FontDialog Done TODO: FontSize

--- Clock/FontDialog.cs
+++ Clock/FontDialog.cs
@@ -15,9 +15,11 @@ namespace Clock
 	public partial class FontDialog : Form
 	{
 		public Font Font {  get; set; }
+		int lastChosenIndex;
 		public FontDialog()
 		{
 			InitializeComponent();
+			lastChosenIndex = 0;
 			LoadFonts("*.ttf");
 			LoadFonts("*.otf");
 		}
@@ -75,6 +77,13 @@ namespace Clock
 		private void buttonOK_Click(object sender, EventArgs e)
 		{
 			this.Font = labelExample.Font;
+			this.lastChosenIndex = comboBoxFont.SelectedIndex;
+		}
+
+		private void buttonCancel_Click(object sender, EventArgs e)
+		{
+			labelExample.Font = this.Font;
+			comboBoxFont.SelectedIndex = lastChosenIndex;
 		}
 	}
 }
