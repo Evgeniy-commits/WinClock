658ec9aa24f769650880bae06894e951e8dc1ed2
Admin <Admin@DESKTOP-8URQBFL>
2026-01-16 23:39:06 +0500

    Alarms fire File only

--- Clock/AlarmDialog.cs
+++ Clock/AlarmDialog.cs
@@ -42,7 +42,7 @@ namespace Clock
 				dtpDate.Value = Alarm.Date;
 				checkBoxUseDate.Checked = true;
 			}
-			dtpTime.Value = Alarm.Time;
+			dtpTime.Value = DateTime.Now.Date + Alarm.Time;
 			Alarm.Days.Extract(clbWeekDays); 
 			labelFilename.Text = Alarm.Filename;
 		}
@@ -95,7 +95,7 @@ namespace Clock
 		private void buttonOK_Click(object sender, EventArgs e)
 		{
 			Alarm.Date = checkBoxUseDate.Checked ? dtpDate.Value : DateTime.MaxValue;
-			Alarm.Time = dtpTime.Value;
+			Alarm.Time = dtpTime.Value.TimeOfDay;
 			Alarm.Days = new Week(checkBoxUseDate.Checked ? (byte) 0 : GetDaysMask());
 			Alarm.Filename = labelFilename.Text;
 		}
